6c2f3b2c2c3f888801c00a1a7706b4f9117e70b1
Nargis <nargissamatova16@gmail.com>
2024-03-25 13:36:02 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
 import './App.css'
 import './Box.css'
 import Box from './Box'
+import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+const [counter, setCounter] = useState('hi')
 
   return (
     <>
+  
+    <p>counter: {counter}</p>
     <Box name='Nargis' title='student' location='Helsinki' />
     <Box name='Blabla' title='someone' location='Helsinki' />
     <Box name='Someone' title='student' location='somewhere' />
